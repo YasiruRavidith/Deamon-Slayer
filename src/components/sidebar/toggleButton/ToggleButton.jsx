6ec44a30327769a22bcf736b5da0dcdc4c2dfd20
YasiruRavidith <yasiruravidith123@gmail.com>
2025-03-02 00:09:35 +0530

    paths corection

--- src/components/sidebar/toggleButton/ToggleButton.jsx
+++ src/components/sidebar/toggleButton/ToggleButton.jsx
@@ -1,7 +1,7 @@
 import {motion} from "framer-motion";
 const ToggleButton = ({setOpen}) => {
     return <button onClick={({})=> setOpen((prev)=> !prev)}  >
-        <img src="public\images_bg.png" alt=""/>
+        <img src="\images_bg.png" alt=""/>
     
     <svg width="23" height="23" viewBox="0 23 23" className="pos">
         <motion.path strokeWidth="3" stroke="white" strokeLinecap="round" variants={{closed:{d:"M 2 2.5 L 20 2.5"},open:{d:"M 3 16.5 L 17 2.5"}}}/>
