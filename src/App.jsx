a5588e117e2e0fff9965f785c3d8e28fb5ab3a7f
YasirRavidith <yasiruravidith123@gmail.com>
2025-09-03 20:51:25 +0530

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./app.scss"
 import Hero from "./components/Main_Characters/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
-import Parallax from "./components/parallax/Parallax";
+import Parallax from "./components/parallax/parallax";
 import Services from "./components/services/Services";
 import Portfolio from "./components/portfolio/Portfolio";
 import Contact from "./components/contact/Contact";
